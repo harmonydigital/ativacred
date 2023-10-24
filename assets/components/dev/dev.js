4ac592c3be10f761385768273771172796b05a40
harmony <harmony.dgt@gmail.com>
2023-10-24 13:40:01 -0300

    '..'

---
+++ assets/components/dev/dev.js
@@ -0,0 +1,15 @@
+body=document.getElementsByTagName('body')[0]
+
+console.log('Body',body)
+
+
+body.innerHTML=`
+    <div style="
+    text-align: center;
+    margin: 60px;
+">
+        <h1>Harmony Building...<h1>
+        <p>Aplicação em desenvolvimento...</p>
+    </div>
+
+`
