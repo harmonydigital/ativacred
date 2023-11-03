63d4291a5a8f274ce55e1e50f87707e9b120bdaa
harmony <harmony.dgt@gmail.com>
2023-11-02 23:16:41 -0300

    '...'

--- assets/components/validate/index.js
+++ assets/components/validate/index.js
@@ -1,10 +1,26 @@
 var getBtn=document.querySelectorAll('.getValidation')
+var contair=document.getElementById('validation')
 
 var arrAct=Array.from(getBtn)
 
+contair.innerHTML=`
 
+<div class='user-area rtrt'> 
+<div class='container-flx'>  
+<div class=''>  
+ <span>Busque restrições em seu CPF.<span>
+</div>
+<div onclick='toggleLogin()' class=''>  
+<i class="fa-regular fa-user"></i>Entrar
+</div> 
+</div> 
+</div> 
+
+`
 arrAct.map((dl)=>{
     dl.addEventListener('click',()=>{
-        alert('don')
+         
+
+        contair.classList.toggle('show')
     })
 })
