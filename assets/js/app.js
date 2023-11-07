6bbd976040a53406a26b0ddd66229a0e70a4107a
harmony <harmony.dgt@gmail.com>
2023-11-07 20:31:10 -0300

    '..'

--- assets/js/app.js
+++ assets/js/app.js
@@ -164,9 +164,7 @@ initapp=()=>{
             <button><i class="fa-solid fa-arrow-right"></i></button>
 
           </div>
-          <div class="returnMsg"> 
-                           <svg class="svg-inline--fa fa-circle-info" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-info" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"></path></svg><!-- <i class="fa-solid fa-circle-info"></i> Font Awesome fontawesome.com --> Saiba mais sobre suas restrições clicando aqui
-                           </div>
+           
       </div>
 
        
